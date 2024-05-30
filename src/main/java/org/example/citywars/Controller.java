a58518b6700a5a1609f1600161d28de74135bd95
Mahdi <mahdi.ghalami.12.28@gmail.com>
2024-05-30 23:03:40 +0330

    1

--- src/main/java/org/example/citywars/Controller.java
+++ src/main/java/org/example/citywars/Controller.java
@@ -1,11 +1,14 @@
 package org.example.citywars;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
+
+    Parent root;
 
     @FXML
     protected void onHelloButtonClick() {
