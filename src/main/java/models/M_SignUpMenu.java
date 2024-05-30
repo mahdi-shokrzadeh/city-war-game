ef58778da6693595619ffd49ed48f4213c28de69
Mahdi <mahdi.ghalami.12.28@gmail.com>
2024-05-30 23:03:40 +0330

    1

---
+++ src/main/java/models/M_SignUpMenu.java
@@ -0,0 +1,10 @@
+package models;
+
+public class M_SignUpMenu extends Menu {
+    public M_SignUpMenu(){
+        super("hello-view");
+    }
+    public Menu myMethods(String input){
+        return null;
+    }
+}
--- src/main/java/org/example/citywars/HelloApplication.java
+++ src/main/java/org/example/citywars/HelloApplication.java
@@ -4,14 +4,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import models.M_SignUpMenu;
+import models.Menu;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class HelloApplication extends Application {
+    Menu menu;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+        menu = new M_SignUpMenu();
+
+        Scanner sc = new Scanner(System.in);//for console version
+        String input;//for console version
+
+//        boolean isEnd = false;
+//        while (!isEnd) {
+//            input = sc.nextLine();
+//            if (input.matches("^ *show +current +menu *$")) {
+//                menu.myNameIs();
+//            } else {
+//                menu = menu.myMethods(input);
+//                if (menu == null) {
+//                    isEnd = true;
+//                }
+//            }
+//        }
+
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(menu.background.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
