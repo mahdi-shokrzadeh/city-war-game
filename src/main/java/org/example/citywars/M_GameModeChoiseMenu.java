ef4c5b7bd813b6806753dd6201a77344e4823be1
Mahdi <mahdi.ghalami.12.28@gmail.com>
2024-05-31 14:44:27 +0330

    real 1

---
+++ src/main/java/org/example/citywars/M_GameModeChoiseMenu.java
@@ -0,0 +1,10 @@
+package org.example.citywars;
+
+public class M_GameModeChoiseMenu extends Menu{
+    public M_GameModeChoiseMenu(){
+        super("M_GameModeChoiseMenu");
+    }
+    public Menu myMethods(String input){
+        return null;
+    }
+}
---
+++ src/main/java/org/example/citywars/M_GamePlayMenu.java
@@ -0,0 +1,10 @@
+package org.example.citywars;
+
+public class M_GamePlayMenu extends Menu{
+    public M_GamePlayMenu(){
+        super("M_GamePlayMenu");
+    }
+    public Menu myMethods(String input){
+        return null;
+    }
+}
