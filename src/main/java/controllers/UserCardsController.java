73c521a75664ed01eadf3aed0c2e551606f7a354
Parsa Joneidi <parsajoneidy@gmail.com>
2024-06-30 17:32:19 +0330

    added id to userCard and changed getAllUsersCards

--- src/main/java/controllers/UserCardsController.java
+++ src/main/java/controllers/UserCardsController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import database.Database;
 import models.User;
@@ -15,7 +17,6 @@ public class UserCardsController {
     public static Response buyCard(int userID, Card card){
         Database<User> userDB = new Database<>("users");
         User user;
-
         try {
             user = userDB.getOne(userID);
         }catch (Exception e){
@@ -25,20 +26,25 @@ public class UserCardsController {
         if( user == null ){
             return new Response("could not find any user with this id",-400);
         }
+        if( user.getCoins() < card.getPrice() ){
+            return new Response("user does not have enough coins to buy this card",-400);
+        }
 
         Database<UserCard> userCardsDB = new Database<>("userCards");
         UserCard userCard = new UserCard(userID, card.getId());
-        userCardsDB.create(userCard);
-        // SAVE THE CARDID IN THE USER AS WELL
+        int id = userCardsDB.create(userCard);
+        user.addUserCardID(id);
+        userDB.update(user, user.getId());
+
         return new Response("card purchased successfully",200);
     }
 
     public static Response getUsersCards(int userID){
         Database<UserCard> userCardDatabase = new Database<>("userCards");
         Database<User> usersDB = new Database<>("users");
+        Database<Card> cardDB = new Database<>("cards");
         User user;
         List<UserCard> allUsersCards;
-
 
         try{
             user = usersDB.getOne(userID);
@@ -60,7 +66,24 @@ public class UserCardsController {
             return new Response("could not find users cards",-400);
         }
 
-        return new Response("all users' cards fetched successfully",200,allUsersCards);
+        List<Card> cards = null;
+
+        try{
+            for(UserCard userCard: allUsersCards){
+                Card card = cardDB.getOne(userCard.getCardID());
+                if( card == null ) { return new Response("a deep error happened while fetching one of the cards",-400); };
+                cards.add(card);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Response("a deep exception happened while fetching cards",-500);
+        }
+
+        if( cards == null ){
+            return new Response("no cards where found for this user",-400);
+        }
+
+        return new Response("all users' cards fetched successfully",200,cards);
     }
 
     public static Response removeUserCard(int userID, Card card){
@@ -78,14 +101,24 @@ public class UserCardsController {
             return new Response("no user found with this id",-400);
         }
 
+        UserCard userCard = null;
+
         try{
-            userCardDatabase.delete(card.getId());
+            Map<String, String> conditions = new HashMap<>();
+            conditions.put("userID",String.valueOf(user.getId()));
+            conditions.put("cardID",String.valueOf(card.getId()));
+            userCard = userCardDatabase.firstWhereEquals(conditions);
+            userCardDatabase.firstDeleteWhereEquals(conditions);
         }catch (Exception e){
             e.printStackTrace();
             return new Response("an exception happend while deleting user card",-500);
         }
+        if( userCard == null ){
+            return new Response("no user card was found",-400);
+        }
 
-        // SAVE THE CHANGES IN THE USER AS WELL
+        user.removeCardID(userCard.getID());
+
         return new Response("user card successfully deleted",200);
     }
 
