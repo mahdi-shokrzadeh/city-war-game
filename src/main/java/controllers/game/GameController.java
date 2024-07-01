031bf611dc7ec8dc441d381afcc1c9e2a5a5fba4
Parsa Joneidi <parsajoneidy@gmail.com>
2024-07-01 14:16:52 +0330

    renamed getID method for models

--- src/main/java/controllers/game/GameController.java
+++ src/main/java/controllers/game/GameController.java
@@ -1,11 +1,27 @@
 package controllers.game;
 
+import database.Database;
 import models.game.Game;
+import models.Response;
+
+import java.util.ResourceBundle;
 
 public class GameController  {
 
     public GameController() {
         // super();
+    }
+
+    public static Response createGame(int p1ID, int p2ID, String mode, String createdAt, int numberOfRounds){
+        Database<Game> gameDB = new Database<>("games");
+        Game game;
+        try{
+            //game = new Game(p1ID, p2ID, mode);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Response("an exception happened while creating game",-500);
+        }
+        return new Response("",0);
     }
 
 }
