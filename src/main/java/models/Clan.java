f8d55c40181fd9b8a2e47a77d2d89017dce0273d
Parsa Joneidi <parsajoneidy@gmail.com>
2024-07-02 21:36:52 +0330

    fix bug in clan model

--- src/main/java/models/Clan.java
+++ src/main/java/models/Clan.java
@@ -1,5 +1,6 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,13 +9,14 @@ public class Clan {
     private final String name;
     private String joiningKey;
     private String battleKey;
-    private int leaderID;
+    private final int leaderID;
     private List<User> members;
-    private ClanBattle battle;
+    private Integer battleID;
     public Clan(int id,String _name){
         name = _name;
-        battle = null;
+        battleID = null;
         leaderID = id;
+        members = new ArrayList<>();
     }
     public int getID(){ return id; }
     public void setID(int _id){ id = _id;}
@@ -23,6 +25,7 @@ public class Clan {
     public String getBattleKey(){ return  battleKey;}
     public List<User> getMembers(){ return members;}
     public int getLeaderID(){ return leaderID;}
+    public Integer getBattleID(){ return battleID; }
     public void setJoiningKey(String key){ joiningKey = key; }
     public void setBattleKey(String key){
         battleKey = key;
@@ -33,11 +36,11 @@ public class Clan {
     public void removeMember(User user){
         members.remove(user);
     }
-    public void startBattle(Clan clan){
-        battle = new ClanBattle(this.getID(), clan.getID(), Math.min(this.getMembers().size(),clan.getMembers().size()));
+    public void startBattle(Integer _battleID){
+        battleID = _battleID;
     }
-    public void endBattle(){ battle.endBattle(); battle = null; }
-
-
+    public void endBattle(){
+        battleID = null;
+    }
 
 }
