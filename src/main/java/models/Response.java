7a4be5983e2e02e37d41dde99538d708b5cb015d
Parsa Joneidi <parsajoneidy@gmail.com>
2024-07-03 19:33:36 +0330

    add a new constructor which takes exception to Response

--- src/main/java/models/Response.java
+++ src/main/java/models/Response.java
@@ -10,11 +10,24 @@ public class Response {
     public String message;
     public int status;
     public boolean ok;
+    public Exception exception;
     public Map<String, Object> body;
 
     public Response(String _message, int _status) {
         status = _status;
         message = _message;
+        exception = null;
+        if (_status < 0) {
+            ok = false;
+        } else {
+            ok = true;
+        }
+    }
+
+    public Response(String _message, int _status, Exception e) {
+        status = _status;
+        message = _message;
+        exception = e;
         if (_status < 0) {
             ok = false;
         } else {
@@ -25,6 +38,7 @@ public class Response {
     public Response(String _message, int _status,String key, Object _body) {
         status = _status;
         message = _message;
+        exception = null;
         body = new HashMap<>();
         body.put(key, _body);
         if (_status < 0) {
@@ -37,6 +51,7 @@ public class Response {
     public Response(String _message, int _status, Map<String, Object> map){
         status = _status;
         message = _message;
+        exception = null;
         body = map;
         if (_status < 0) {
             ok = false;
