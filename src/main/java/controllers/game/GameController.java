9b0abd1062adbf1c7b79de2c9c0e5459de0c540b
Parsa Joneidi <parsajoneidy@gmail.com>
2024-07-05 09:40:12 +0330

    fix bug in game controller

--- src/main/java/controllers/game/GameController.java
+++ src/main/java/controllers/game/GameController.java
@@ -1,44 +1,304 @@
 package controllers.game;
 
 import controllers.ClanController;
-import database.DBs.GameDB;
-import database.DBs.ClanBattleDB;
-import database.DBs.ClanDB;
-import models.Clan;
-import models.ClanBattle;
-import models.User;
+import controllers.UserCardsController;
+import database.DBs.*;
+import models.*;
+import models.card.Card;
 import models.game.Game;
-import models.Response;
 
 import java.util.*;
 import java.util.stream.Stream;
 
 public class GameController {
+    private static final UserDB userDB = new UserDB();
     private static final GameDB gameDB = new GameDB();
     private static final ClanDB clanDB = new ClanDB();
     private static final ClanBattleDB cbDB = new ClanBattleDB();
-    public static Response createGame(User p1, User p2, String mode, int numberOfRounds, String winner){
+    private static final UserCardDB ucDB = new UserCardDB();
+    private static int calculateWinnerScore(User user, int hp, int n){
+        int temp = hp - user.getHitPoints() == 0 ? 1 : hp - user.getHitPoints();
+        return (user.getDamage() + temp) / n;
+    }
+    private static int calculateLoserScore(User user, int hp, int n){
+        return (user.getDamage()/10 + 1000/hp) * n;
+    }
+    private static int calculateUserLevel(User user){
+        int sum = 0;
+        int level = user.getLevel();
+        while ( sum <= user.getExperience() ) {
+            sum = (int) (level * (level + 1) * 0.5);
+            user.setExperience( user.getExperience() - 50*sum );
+            level++;
+        }
+        return Math.max(level, 1);
+    }
+    private static int calculateCardLevel(UserCard uc){
+        int sum = 0;
+        int level = uc.getLevel();
+        while ( sum < uc.getExperience() ){
+            sum = (int) (uc.getLevel() * (uc.getLevel() + 1) * 0.5);
+            uc.setExperience( uc.getExperience() - 50*sum );
+            level++;
+        }
+        level = Math.max(level,1);
+        level = Math.min(level, 3);
+        return level;
+    }
+    public static Response createGame(User p1, User p2, int numberOfRounds, String winnerString, List<Card> p1Cards, List<Card> p2Cards){
         Game game  = null;
-        try{
-            game = new Game(p1, p2, mode);
-            game.setNumber_of_rounds(numberOfRounds);
-            if( winner.equals("p1") ) {
-                game.setWinner("p1");
-            }else if( winner.equals("p2") ){
-                game.setWinner("p2");
+        int p1OriginalHP = userDB.getOne(p1.getID()).getHitPoints();
+        int p2OriginalHP =  userDB.getOne(p2.getID()).getHitPoints();
+        User winnerUser = null;
+        User loserUser = null;
+        int winnerOriginalHP;
+        int winnerScore;
+        int loserScore;
+        int p1Score;
+        int p2Score;
+        Exception exception = null;
+        Map<String, Object> result = new HashMap<>();
+        String winnerReward = "";
+        String loserReward = "";
+        List<Card> allP1Cards = ((List<Card>) UserCardsController.getUsersCards(p1.getID()).body.get("cards"));
+        List<Card> allP2Cards = ((List<Card>) UserCardsController.getUsersCards(p2.getID()).body.get("cards"));
+
+        if( !winnerString.equals("player_one") && !winnerString.equals("player_two")){
+            return new Response("invalid winner",-422);
+        }
+        if( winnerString.equals("player_one") ){
+            winnerScore = calculateWinnerScore(p1,p1OriginalHP,numberOfRounds);
+            loserScore = calculateLoserScore(p2,p2OriginalHP,numberOfRounds);
+            p1Score = winnerScore;
+            p2Score = loserScore;
+            winnerUser = p1;
+            loserUser = p2;
+            winnerOriginalHP = p1OriginalHP;
+        }else {
+            winnerScore = calculateWinnerScore(p2,p2OriginalHP, numberOfRounds);
+            loserScore = calculateLoserScore(p1,p1OriginalHP,numberOfRounds);
+            p2Score = winnerScore;
+            p1Score = loserScore;
+            winnerUser = p2;
+            loserUser = p1;
+            winnerOriginalHP = p2OriginalHP;
+        }
+
+        allP1Cards.removeAll(p1Cards);
+        for(Card card: allP1Cards){
+            try {
+                UserCard userCard = (UserCard) UserCardsController.getUserCard(p1.getID(), card.getID()).body.get("userCard");
+                userCard.setExperience(userCard.getExperience() + p1Score);
+                int level = calculateCardLevel(userCard);
+                if(winnerString.equals("player_one")) {
+                    winnerReward += "card " + card.getName() + ": +" + (level - userCard.getLevel()) + " level\n";
+                }else{
+                    loserReward += "card " + card.getName() + ": +" + (level - userCard.getLevel()) + " level\n";
+                }
+                userCard.setLevel(level);
+                ucDB.update(userCard, userCard.getID());
+                if( p1.getGameCharacter().getName().equals(card.getCharacter().getName()) ){
+                    p1Score += 100 * userCard.getLevel();
+                }
+            }catch (Exception e){
+                exception = e;
             }
+        }
+        allP2Cards.removeAll(p2Cards);
+        for(Card card: allP2Cards){
+            try {
+                UserCard userCard = (UserCard) UserCardsController.getUserCard(p2.getID(), card.getID()).body.get("userCard");
+                userCard.setExperience(userCard.getExperience() + p2Score);
+                int level = calculateCardLevel(userCard);
+                if(winnerString.equals("player_two")){
+                    winnerReward += "card " + card.getName() + ": +" + (level - userCard.getLevel()) + " level\n";
+                }else{
+                    loserReward += "card " + card.getName() + ": +" + (level - userCard.getLevel()) + " level\n";
+                }
+                userCard.setLevel(level);
+                ucDB.update(userCard, userCard.getID());
+                if( p2.getGameCharacter().getName().equals(card.getCharacter().getName()) ){
+                    p2Score += 100 * userCard.getLevel();
+                }
+            }catch (Exception e){
+                exception = e;
+            }
+        }
+
+        try{
+            game = new Game(p1, p2, "duel");
+            game.setNumber_of_rounds(numberOfRounds);
+            game.setWinner(winnerString);
             game.setEnded_at(new Date().toString());
-            // set game reward
-            // set game bet amount
             gameDB.create(game);
+            result.put("game",game);
         }catch (Exception e){
-            e.printStackTrace();
-            return new Response("an exception happened while creating game",-500);
+            return new Response("an exception happened while creating game",-500,e);
         }
         if( game == null ){
-            return new Response("unable to create game",-400,"game",game);
+            return new Response("unable to create game",-400);
         }
-        return new Response("game created successfully",200,"game",game);
+
+        try{
+            winnerUser.setExperience(winnerUser.getExperience() + winnerScore);
+            winnerReward += "+" + winnerScore + " experience\n";
+            int level = calculateUserLevel(winnerUser);
+            winnerReward += "+" + (level - winnerUser.getLevel()) + " level\n";
+            winnerUser.setLevel(level);
+            winnerUser.setHitPoints(winnerOriginalHP + 25);
+            winnerReward += "+25 hit points\n";
+            int coins = winnerUser.getCoins() + (winnerScore/10);
+            winnerReward += "+" + (coins - winnerUser.getCoins()) + " coins\n";
+            winnerUser.setCoins(coins);
+            loserUser.setExperience(loserUser.getExperience() + loserScore );
+            loserReward += "+" + loserScore + " experience\n";
+            userDB.update(winnerUser, winnerUser.getID());
+            userDB.update(loserUser, loserUser.getID());
+        }catch (Exception e){
+            return new Response("an exception happened while saving users",-500,e);
+        }
+
+        result.put("winner", winnerReward);
+        result.put("loser", loserReward);
+        if( exception != null ){
+            return new Response("an exception happened while upgrading a card",-500,exception);
+        }
+        return new Response("game created successfully",200,result);
+
+    }
+
+    public static Response createBotGame(User player, int numberOfRounds, String winner,  List<Card> cards){
+        Game game  = null;
+        int playerOriginalHP = userDB.getOne(player.getID()).getHitPoints();
+        Map<String, Object> result = new HashMap<>();
+        String winnerReward = "";
+        String loserReward = "";
+        Exception exception = null;
+        int score = winner.equals("player_two") ? calculateWinnerScore(player, playerOriginalHP,numberOfRounds) : calculateLoserScore(player, playerOriginalHP, numberOfRounds);
+
+        if( !winner.equals("player_two") && !winner.equals("bot")){
+            return new Response("invalid winner", -422);
+        }
+
+        try{
+            game = new Game(player,"AI");
+            game.setNumber_of_rounds(numberOfRounds);
+            game.setWinner(winner);
+            game.setEnded_at(new Date().toString());
+            gameDB.create(game);
+        }catch (Exception e){
+            return new Response("an exception happened while creating game",-500,e);
+        }
+        if( game == null ){
+            return new Response("unable to create game",-400);
+        }
+
+        List<Card> allP1Cards = ((List<Card>) UserCardsController.getUsersCards(player.getID()).body.get("cards"));
+        allP1Cards.removeAll(cards);
+        for(Card card: allP1Cards){
+            try {
+                UserCard userCard = (UserCard) UserCardsController.getUserCard(player.getID(), card.getID()).body.get("userCard");
+                userCard.setExperience(userCard.getExperience() + score);
+                int level = calculateCardLevel(userCard);
+                if( winner.equals("player_two") ){
+                    winnerReward += "card " + card.getName() + ": +" + (level - userCard.getLevel()) + " level\n";
+                }else{
+                    loserReward += "card " + card.getName() + ": +" + (level - userCard.getLevel()) + " level\n";
+                }
+                userCard.setLevel(calculateCardLevel(userCard));
+                ucDB.update(userCard, userCard.getID());
+                if( player.getGameCharacter().getName().equals(card.getCharacter().getName()) ){
+                    score += 100 * userCard.getLevel();
+                }
+            }catch (Exception e){
+                exception = e;
+            }
+        }
+
+        try{
+            if( winner.equals("player_two") ){
+                score = calculateWinnerScore(player,playerOriginalHP,numberOfRounds);
+                player.setExperience(player.getExperience() + score);
+                winnerReward += "+" + score + " experience\n";
+                int level = calculateUserLevel(player);
+                winnerReward += "+" + (level - player.getLevel()) + " level\n";
+                player.setLevel(level);
+                player.setHitPoints(playerOriginalHP + 25);
+                winnerReward += "+25 hit points\n";
+                int coins = player.getCoins() + (score/10);
+                winnerReward += "+" + (coins - player.getCoins()) + " coins\n";
+                player.setCoins(coins);
+                if (player.getProgress() == 5) {
+                    player.resetProgress();
+                }else {
+                    player.makeProgress();
+                }
+            }else{
+                score = calculateLoserScore(player,playerOriginalHP,numberOfRounds);
+                player.setExperience(player.getExperience() + score);
+                loserReward += "+" + score + " experience\n";
+                int level = calculateUserLevel(player);
+                player.setLevel(level);
+                loserReward += "+" + level + " level\n";
+                player.setHitPoints(playerOriginalHP);
+            }
+            userDB.update(player, player.getID());
+        }catch (Exception e){
+            return new Response("an exception happened while saving user",-500,e);
+        }
+
+        result.put("winner", winnerReward);
+        result.put("loser", loserReward);
+        if( exception != null ){
+            return new Response("an exception happened while upgrading a card",-500,exception);
+        }
+        return new Response("game created successfully",200,result);
+    }
+
+    public static Response createGambleGame(User p1, User p2, int numberOfRounds, String winner, int betAmount){
+        Game game  = null;
+        int p1OriginalHP = userDB.getOne(p1.getID()).getHitPoints();
+        int p2OriginalHP = userDB.getOne(p2.getID()).getHitPoints();
+        String winnerReward = "+" + betAmount + " coins\n";
+        String loserReward = "-" + betAmount + " coins\n";
+        Map<String, Object> result = new HashMap<>();
+
+        if( !winner.equals("player_one") && !winner.equals("player_two")){
+            return new Response("invalid winner",-422);
+        }
+
+        try{
+            game = new Game(p1, p2, "duel");
+            game.setNumber_of_rounds(numberOfRounds);
+            game.setWinner(winner);
+            game.setEnded_at(new Date().toString());
+            gameDB.create(game);
+        }catch (Exception e){
+            return new Response("an exception happened while creating game",-500,e);
+        }
+        if( game == null ){
+            return new Response("unable to create game",-400);
+        }
+
+        try{
+            if( winner.equals("player_one") ){
+                p1.setCoins( p1.getCoins() + betAmount );
+                p2.setCoins( p2.getCoins() - betAmount );
+            }else {
+                p1.setCoins( p1.getCoins() - betAmount );
+                p2.setCoins( p2.getCoins() + betAmount );
+            }
+            p1.setHitPoints( p1OriginalHP );
+            p2.setHitPoints( p2OriginalHP );
+            userDB.update(p1, p1.getID());
+            userDB.update(p2, p2.getID());
+        }catch (Exception e){
+            return new Response("an exception happened while saving users",-500,e);
+        }
+
+        result.put("winner", winnerReward);
+        result.put("loser", loserReward);
+        return new Response("game created successfully",200,result);
     }
 
     public static Response getAllUserGames(int id){
@@ -63,8 +323,7 @@ public class GameController {
         try{
             battle = cbDB.firstWhereEqualsOr(userClan.getID());
         }catch (Exception e){
-            System.out.println(e.getMessage());
-            return new Response("an exception happened while fetching battle",-500);
+            return new Response("an exception happened while fetching battle",-500,e);
         }
         if( battle == null ){
             return new Response("no battle was found for your clan",-400);
@@ -123,13 +382,13 @@ public class GameController {
         return new Response("clan game essentials fetched successfully",200,result);
     }
 
-    public static Response creatGameClan(ClanBattle battle, Clan attackerClan, Clan defenderClan, User p1, User p2, String mode, int numberOfRounds, String winner){
+    public static Response creatGameClan(ClanBattle battle, Clan attackerClan, Clan defenderClan, User p1, User p2, int numberOfRounds, String winner, List<Card> p1Cards, List<Card> p2Cards){
 
-        Game game= (Game)createGame(p1, p2, mode,numberOfRounds,winner).body.get("game");
-        if( game == null ){
+        Response res = createGame(p1, p2,numberOfRounds,winner, p1Cards, p2Cards);
+        if( res.body.get("game") == null ){
             return new Response("unable to create game",-400);
         }
-        battle.playAGame(game);
+        battle.playAGame((Game)res.body.get("game"));
         if( battle.getNumberOfRemainingGames() == 0 ){
             boolean hasFinale = battle.endBattle();
             if( !hasFinale ) {
@@ -137,11 +396,15 @@ public class GameController {
                 defenderClan.endBattle();
             }
         }
-        clanDB.update(attackerClan, attackerClan.getID());
-        clanDB.update(defenderClan, defenderClan.getID());
-        cbDB.update(battle,battle.getID());
+        try {
+            clanDB.update(attackerClan, attackerClan.getID());
+            clanDB.update(defenderClan, defenderClan.getID());
+            cbDB.update(battle, battle.getID());
+        }catch (Exception e){
+            return new Response("an exception happened while creating battle",-500,e);
+        }
 
-        return new Response("game created successfully",200);
+        return new Response("game created successfully",200,res.body);
     }
 
 }
