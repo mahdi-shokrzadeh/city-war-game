84af86130a9f685af0496d904f967afa5f01cfa6
Mahdi <mahdi.ghalami.12.28@gmail.com>
2024-07-05 11:27:36 +0330

    second person login (incomplete!!!)

--- src/main/java/org/example/citywars/M_LoginMenu.java
+++ src/main/java/org/example/citywars/M_LoginMenu.java
@@ -56,7 +56,7 @@ public class M_LoginMenu extends Menu {
                 System.out.println(s.message);
                 if (s.ok){
                     if (secondPersonNeeded) {
-                        if (s.body.get("user").equals(loggedInUser)){
+                        if (((User)s.body.get("user")).getID()==loggedInUser.getID()){
                             System.out.println("\n!!! Same user, login again !!!\n");
                             return this;
                         } else
@@ -74,7 +74,11 @@ public class M_LoginMenu extends Menu {
                 System.out.println(s.message);
                 if (s.ok){
                     if (secondPersonNeeded) {
-                        return new Game(loggedInUser,(User)s.body.get("user"),"duel");
+                        if (((User)s.body.get("user")).getID()==loggedInUser.getID()){
+                            System.out.println("\n!!! Same user, login again !!!\n");
+                            return this;
+                        } else
+                            return new Game(loggedInUser,(User)s.body.get("user"),"duel");
                     }
                     else {
                         loggedInUser=(User)s.body.get("user");
