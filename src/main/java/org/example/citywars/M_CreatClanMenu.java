ecc0f8a7b97de1310ade7146e1fbe0ae7696833b
Parsa Joneidi <parsajoneidy@gmail.com>
2024-07-06 18:35:36 +0330

    fix bug in create clan menu

--- src/main/java/org/example/citywars/M_CreatClanMenu.java
+++ src/main/java/org/example/citywars/M_CreatClanMenu.java
@@ -5,12 +5,14 @@ import models.Response;
 
 import java.util.regex.Pattern;
 
-public class M_CreatClanMenu extends Menu{
-    public M_CreatClanMenu(){
+public class M_CreatClanMenu extends Menu {
+    public M_CreatClanMenu() {
         super("M_ClanMainMenu");
-        //patterns.add(Pattern.compile("^ *-Create +a +clan +(?<clanName>[\\S ]+) *$"));
+        // patterns.add(Pattern.compile("^ *-Create +a +clan +(?<clanName>[\\S ]+)
+
     }
-    private void printMenu(){
+
+    private void printMenu() {
         System.out.println("CREATE CLAN");
         System.out.println("Options: ");
         System.out.println("    Back");
@@ -18,31 +20,30 @@ public class M_CreatClanMenu extends Menu{
         System.out.println("    You can create a clan using the following command: ");
         System.out.println("        create clan -n <name>");
     }
-    public Menu myMethods(){
+
+    public Menu myMethods() {
         printMenu();
         do {
             String input = consoleScanner.nextLine().trim();
-            if(input.matches("^create clan -n (?<name>[a-zA-Z]+)$")){
+            if (input.matches("^create clan -n (?<name>[a-zA-Z]+)$")) {
                 matcher = Pattern.compile("^create clan -n (?<name>[a-zA-Z]+)$").matcher(input);
                 matcher.find();
-                try{
+                try {
                     Response res = ClanController.createClan(loggedInUser, matcher.group("name"));
                     System.out.println(res.message);
-                    if( res.exception != null ){
+                    if (res.exception != null) {
                         System.out.println(res.exception.getMessage());
                     }
-                }catch (Exception e){
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
-            }else if(input.matches("show current menu")){
+            } else if (input.matches("show current menu")) {
                 System.out.println("You are currently at " + getName());
-            }
-            else if(input.matches("^Back$")){
+            } else if (input.matches("^Back$")) {
                 return new M_ClanMainMenu();
-            }
-            else {
+            } else {
                 System.out.println("invalid command!");
             }
-        }while (true);
+        } while (true);
     }
 }
