dc002925be12c859421c6342f5c8a4aa5f32c811
Parsa Joneidi <parsajoneidy@gmail.com>
2024-07-09 22:05:54 +0330

    fix bug in card controller

--- src/main/java/controllers/CardController.java
+++ src/main/java/controllers/CardController.java
@@ -164,8 +164,12 @@ public class CardController {
             return new Response("no character was found with this name", -400);
         }
 
+        if (imageURL == null || imageURL.isEmpty()) {
+            return new Response("image can not be empty", -422);
+        }
+
         Card card = new Card(name, price, duration, type, power, damage, upgradeLevel, upgradeCost, "desc",
-                gameCharacter);
+                gameCharacter, imageURL);
         try {
             cardDB.create(card);
         } catch (Exception e) {
@@ -267,7 +271,81 @@ public class CardController {
         Card card = new Card(name, price, duration, existingCard.getCardType().toString(), power, damage, upgradeLevel,
                 upgradeCost, desc, gameCharacter);
         try {
-            cardDB.update(card, card.getID());
+            cardDB.update(card, cardID);
+        } catch (Exception e) {
+            return new Response("an exception happened while updating card", -500, e);
+        }
+
+        res = new Response("card updated successfully", 200);
+        return res;
+
+    }
+
+    public static Response editCard(User user, int cardID, String name, int price, int duration, int power, int damage,
+            int upgradeLevel, int upgradeCost, String desc, String characterName, String imageURL) {
+        Response res;
+
+        if (!user.getRole().equals("admin")) {
+            res = new Response("only admins can add cards", -401);
+            return res;
+        }
+        if (name.isEmpty()) {
+            res = new Response("invalid card name", -422);
+            return res;
+        }
+        if (price < 0) {
+            res = new Response("card price cannot be negative", -422);
+            return res;
+        }
+        if (duration < 1 || duration > 5) {
+            res = new Response("card duration cannot be negative", -422);
+            return res;
+        }
+        if (desc.isEmpty()) {
+            res = new Response("invalid card description", -422);
+            return res;
+        }
+        if (damage < 10 || damage > 50) {
+            res = new Response("invalid card power", -422);
+            return res;
+        }
+        if (power < 10 || power > 100) {
+            res = new Response("invalid card power", -422);
+            return res;
+        }
+        if (upgradeLevel <= 0) {
+            res = new Response("card upgrade level can not be none-positive", -422);
+            return res;
+        }
+        if (upgradeCost <= 0) {
+            res = new Response("card upgrade cost can not be none-positive", -422);
+            return res;
+        }
+
+        GameCharacter gameCharacter = null;
+        try {
+            gameCharacter = gcDB.getByName(characterName);
+        } catch (Exception e) {
+            return new Response("a deep exception happened while fetching the character", -500, e);
+        }
+        if (gameCharacter == null) {
+            return new Response("no character was found with this name", -400);
+        }
+
+        Card existingCard = cardDB.getOne(cardID);
+        if (existingCard == null) {
+            res = new Response("card with this does name not exist exists", -409);
+            return res;
+        }
+
+        if (imageURL == null || imageURL.isEmpty()) {
+            return new Response("image can not be empty", -422);
+        }
+
+        Card card = new Card(name, price, duration, existingCard.getCardType().toString(), power, damage, upgradeLevel,
+                upgradeCost, desc, gameCharacter, imageURL);
+        try {
+            cardDB.update(card, cardID);
         } catch (Exception e) {
             return new Response("an exception happened while updating card", -500, e);
         }
@@ -294,13 +372,16 @@ public class CardController {
         if (card.getCardType().equals("Spell")) {
             return new Response("", 200);
         }
-        File file = new File(
-                "src/main/resources/Cards/" + card.getCharacter().getName() + "_d" + card.getDuration() + "_l" + level
-                        + ".png");
-        if (!file.isFile()) {
+        File file = null;
+        if (card.getImageURL() == null) {
+            file = new File("src/main/resources/Cards/" + card.getCharacter().getName() + "_d"
+                    + String.valueOf(card.getDuration()) + "_l" + String.valueOf(level) + ".png");
+        } else {
+            file = new File(card.getImageURL());
+        }
+        if (!file.exists()) {
             return new Response("image not found", -404);
         }
-
         Image image = null;
         ImageView imageView = null;
         try {
